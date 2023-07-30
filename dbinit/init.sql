5be3008c8e4c752a894d7c9821f7c24a4976e896
thompa21 <tholind@kth.se>
2023-07-30 14:58:56 +0200

    first

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `books` (
+CREATE TABLE IF NOT EXISTS `newbooks` (
   `id` int(11) NOT NULL,
   `mmsid` varchar(50) NOT NULL,
   `recordid` varchar(100) NOT NULL,
@@ -17,12 +17,12 @@ CREATE TABLE IF NOT EXISTS `books` (
   `category` varchar(500) DEFAULT NULL,
   `subcategory` varchar(500) DEFAULT NULL,
   `booktype` char(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2244 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `books`
+ALTER TABLE `newbooks`
   ADD PRIMARY KEY (`id`);
 
 
-ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2244;
+ALTER TABLE `newbooks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
