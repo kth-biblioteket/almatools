232d360f9af535456f15c133651afe183ae4b1c5
Thomas Lind <tholind@kth.se>
2023-12-12 14:09:48 +0100

    first

--- index.js
+++ index.js
@@ -132,8 +132,9 @@ appRoutes.get("/payment/checkout", async function (req, res, next) {
             //Hämta payment
             //const payment = await Controllers.readPayment(req.query.paymentId)
             const payment = await axios.post(process.env.ALMATOOLSAPI_INTERNAL_ENDPOINT + "v1/checkpayment/" + req.query.paymentId)
-            console.log(payment)
+            
             if( payment.data.length > 0) {
+                console.log(payment.data[0])
                 if (payment.data[0].finished == 1) {
                     almapaymentdata = {
                         "status": "finished",
